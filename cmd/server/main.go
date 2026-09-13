20127419a30bde12a1b3e9b0d2f4138a9b36fad3
MonikaLiu <monika.liu@arangodb.com>
2026-09-12 22:47:53 -0700

    register route for inference with handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,9 +10,9 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	healthHandler := func(w http.ResponseWriter, _ *http.Request) {
+	healthHandler := func(w http.ResponseWriter, _ *http.Request) {}
 
-	}
+	inferenceHandler := func(w http.ResponseWriter, _ *http.Request) {}
 
 	server := http.Server{
 		Addr:              ":8080",
@@ -22,5 +22,6 @@ func main() {
 	}
 
 	mux.HandleFunc("GET /health", healthHandler)
+	mux.HandleFunc("POST /v1/inference", inferenceHandler)
 	log.Fatal(server.ListenAndServe())
 }
